68ff5d75c645cb558f71657df7c8032b134ffa3a
Dave Turner <davetii@gmail.com>
2021-12-16 17:00:15 -0500

    Improving tests

--- src/test/java/software/daveturner/gametimejpa/repo/CoachRepoTest.java
+++ src/test/java/software/daveturner/gametimejpa/repo/CoachRepoTest.java
@@ -1,5 +1,6 @@
 package software.daveturner.gametimejpa.repo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,7 @@ import software.daveturner.gametimejpa.domain.Team;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest(classes = GametimeJpaApplication.class)
 public class CoachRepoTest {
@@ -33,9 +35,12 @@ public class CoachRepoTest {
     @Test
     public void ensureTeamReturnsExpected() {
         Coach coachBob = coachRepo.save(helper.newCoach("Bob", "Jones"));
+        Coach coach = coachRepo.save(coachBob);
+        assertNull(coachRepo.findById(coachBob.getId()).get().getTeam());
+
         Team panthers = helper.newTeam("MI", "Michigan", "Panthers");
         coachBob.setTeam(panthers);
-        Coach coach = coachRepo.save(coachBob);
+        coachRepo.save(coachBob);
         assertEquals(coachRepo.findById(coach.getId()).get().getTeam(), panthers);
     }
 }
