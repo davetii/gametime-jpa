8cced3c471aa6aeeb959d858b413783b0d722bf0
Dave Turner <davetii@gmail.com>
2021-12-17 12:40:13 -0500

    Reworked Preloaded Data tests

--- src/main/resources/preloaded-data-tests.sql
+++ src/main/resources/preloaded-data-tests.sql
@@ -1,5 +1,5 @@
 insert into COACH (ID, FIRST_NAME, LAST_NAME) values(1,'Dave', 'Turner');
-insert into team (id, locale, name) values('MI', 'Michigan', 'Panthers');
-insert into team_coach (team_id, coach_id) values('MI', 1);
+insert into team (id, locale, name, coach_id) values('MI', 'Michigan', 'Panthers', 1);
+
 
 
