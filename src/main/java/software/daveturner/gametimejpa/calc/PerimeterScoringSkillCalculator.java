df99e3ffdf4da0e3a0e6b3bf44c684bc0d7da944
Dave Turner <davetii@gmail.com>
2021-12-22 22:28:37 -0500

    Added Perimeter Skillset

--- src/main/java/software/daveturner/gametimejpa/calc/PerimeterScoringSkillCalculator.java
+++ src/main/java/software/daveturner/gametimejpa/calc/PerimeterScoringSkillCalculator.java
@@ -2,7 +2,6 @@ package software.daveturner.gametimejpa.calc;
 
 import org.springframework.stereotype.Component;
 import software.daveturner.gametimejpa.domain.Player;
-
 import java.math.BigDecimal;
 
 @Component
@@ -10,6 +9,37 @@ public class PerimeterScoringSkillCalculator implements SkillCalculator {
 
     @Override
     public BigDecimal calc(Player player) {
-        return null;
+        double value = ((player.getShotSkill() * 4) +
+                (player.getShotSelection() * 2) +
+                player.getLuck() +
+                player.getIntelligence() +
+                player.getAthleticism() +
+                player.getSpeed()) / 10d;
+
+        if(player.getShotSkill() > 19) { value += 7; }
+        else if(player.getShotSkill() > 18) { value += 5; }
+        else if(player.getShotSkill() > 17) { value += 3.5; }
+        else if(player.getShotSkill() > 17) { value += 3; }
+        else if(player.getShotSkill() > 16) { value += 2.5; }
+        else if(player.getShotSkill() > 15) { value += 2; }
+        else if(player.getShotSkill() > 12) { value += 1; }
+
+        if(player.getShotSkill() < 2) { value -= 8; }
+        else if(player.getShotSkill() < 3) { value -= 6; }
+        else if(player.getShotSkill() < 4) { value -= 5; }
+        else if(player.getShotSkill() < 5) { value -= 4; }
+        else if(player.getShotSkill() < 6) { value -= 3; }
+        else if(player.getShotSkill() < 8) { value -= 2; }
+        else if(player.getShotSkill() < 10) { value -= 1; }
+
+        if(player.getShotSelection() > 18) { value += 4; }
+        else if(player.getShotSelection() > 16) { value += 3; }
+        else if(player.getShotSelection() > 14) { value += 2; }
+
+        if(player.getSize() > 18) { value -= 2; }
+        else if(player.getSize() > 16) { value -= 1.5; }
+        else if(player.getSize() > 14) { value -= 1; }
+
+        return round(value);
     }
 }
--- src/test/java/software/daveturner/gametimejpa/calc/PerimeterScoringSkillCalculatorTest.java
+++ src/test/java/software/daveturner/gametimejpa/calc/PerimeterScoringSkillCalculatorTest.java
@@ -1,6 +1,7 @@
 package software.daveturner.gametimejpa.calc;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class PerimeterScoringSkillCalculatorTest extends SkillSetCalculatorUnitTest {
 
@@ -8,5 +9,22 @@ public class PerimeterScoringSkillCalculatorTest extends SkillSetCalculatorUnitT
     public void setup() {
         calc = new PerimeterScoringSkillCalculator();
         player  = BASE_PLAYER();
+    }
+
+    @Test
+    public void ensureAveragePerimeterReturnsExpected() {
+        assertPlayer(AVERAGE_SKILLSET, calc);
+    }
+
+    @Test
+    public void ensureHighShotSkillReturnsExpected() {
+        player.setShotSkill(20);
+        assertPlayer(21d, calc);
+    }
+
+    @Test
+    public void ensureLowShotSkillReturnsExpected() {
+        player.setShotSkill(1);
+        assertPlayer(-1.6d, calc);
     }
 }
