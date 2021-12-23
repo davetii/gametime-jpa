7d99004bc797dad8d1a4e62b26623e728b5ff951
Dave Turner <davetii@gmail.com>
2021-12-22 22:23:16 -0500

    Added Offense Rebounding

--- src/test/java/software/daveturner/gametimejpa/calc/OffenseReboundSkillCalculatorTest.java
+++ src/test/java/software/daveturner/gametimejpa/calc/OffenseReboundSkillCalculatorTest.java
@@ -1,6 +1,7 @@
 package software.daveturner.gametimejpa.calc;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class OffenseReboundSkillCalculatorTest extends SkillSetCalculatorUnitTest {
 
@@ -8,5 +9,29 @@ public class OffenseReboundSkillCalculatorTest extends SkillSetCalculatorUnitTes
     public void setup() {
         calc = new OffenseReboundSkillCalculator();
         player  = BASE_PLAYER();
+    }
+
+    @Test
+    public void ensureAverageOffenseReboundsReturnsExpected() {
+        assertPlayer(AVERAGE_SKILLSET, calc);
+    }
+
+    @Test
+    public void ensureLowSizeReturnsExpected() {
+        player.setSize(2);
+        assertPlayer(5.7d, calc);
+    }
+
+    @Test
+    public void ensureHighDeterminationReturnsExpected() {
+        player.setDetermination(19);
+        assertPlayer(15.8d, calc);
+    }
+
+    @Test
+    public void ensureBigSizeAndSpeedReturnsExepected() {
+        player.setSize(17);
+        player.setSpeed(14);
+        assertPlayer(14.2d, calc);
     }
 }
