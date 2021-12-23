e10dd979d7e49480b4eeba7f0192ca3ce4151000
Dave Turner <davetii@gmail.com>
2021-12-22 21:13:03 -0500

    Wired up Skill Mapper

--- src/main/java/software/daveturner/gametimejpa/calc/DefenseReboundSkillCalculator.java
+++ src/main/java/software/daveturner/gametimejpa/calc/DefenseReboundSkillCalculator.java
@@ -1,9 +1,11 @@
 package software.daveturner.gametimejpa.calc;
 
+import org.springframework.stereotype.Component;
 import software.daveturner.gametimejpa.domain.Player;
 
 import java.math.BigDecimal;
 
+@Component
 public class DefenseReboundSkillCalculator  implements SkillCalculator{
     @Override
     public BigDecimal calc(Player player) {
