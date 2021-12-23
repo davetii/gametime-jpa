3d347b890d9d8cb7f4b6f74a63de89b0bd6d927b
Dave Turner <davetii@gmail.com>
2021-12-22 22:26:07 -0500

    Added Passing Skillset

--- src/test/java/software/daveturner/gametimejpa/calc/PassingSkillCalculatorTest.java
+++ src/test/java/software/daveturner/gametimejpa/calc/PassingSkillCalculatorTest.java
@@ -1,6 +1,7 @@
 package software.daveturner.gametimejpa.calc;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class PassingSkillCalculatorTest extends SkillSetCalculatorUnitTest {
 
@@ -8,5 +9,34 @@ public class PassingSkillCalculatorTest extends SkillSetCalculatorUnitTest {
     public void setup() {
         calc = new PassingSkillCalculator();
         player  = BASE_PLAYER();
+    }
+
+    @Test
+    public void ensureAveragePassingReturnsExpected() {
+        assertPlayer(AVERAGE_SKILLSET, calc);
+    }
+
+    @Test
+    public void assertGoodHandle() {
+        player.setHandle(18);
+        assertPlayer(17d, calc);
+    }
+
+    @Test
+    public void assertGoodIntel() {
+        player.setIntelligence(18);
+        assertPlayer(16.7d, calc);
+    }
+
+    @Test
+    public void assertBadHandle() {
+        player.setHandle(4);
+        assertPlayer(4d, calc);
+    }
+
+    @Test
+    public void assertBadIntel() {
+        player.setIntelligence(4);
+        assertPlayer(5d, calc);
     }
 }
