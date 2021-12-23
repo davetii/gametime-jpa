13852dffcdd889ff2f62cf087e2e0b5783aaf893
Dave Turner <davetii@gmail.com>
2021-12-22 22:17:08 -0500

    Added Long Range Skill Set

--- src/main/java/software/daveturner/gametimejpa/calc/IndividualDefenseSkillCalculator.java
+++ src/main/java/software/daveturner/gametimejpa/calc/IndividualDefenseSkillCalculator.java
@@ -10,6 +10,83 @@ public class IndividualDefenseSkillCalculator implements SkillCalculator {
 
     @Override
     public BigDecimal calc(Player player) {
-        return null;
+        double value = ((player.getAthleticism() * 3) + (player.getDetermination() * 3) + (player.getIntelligence() * 2) +
+                player.getEgo() + (player.getEndurance() * 2) + player.getHandle() + player.getLuck()) / 13d;
+
+        value += maybeAddIndividualDefense(player);
+        value -= maybeLowerInidividualDefense(player);
+        if(player.getYearsPro() > 12) { value -= 2.5d; }
+        else if ( player.getYearsPro() > 9) { value -= 2d; }
+        else if ( player.getYearsPro() > 6) { value -= 1d; }
+        return round(value);
     }
+
+    private double maybeAddIndividualDefense(Player a) {
+        double result = 0d;
+        int speedStrength = a.getStrength() + a.getSpeed();
+        int strengthSize = a.getStrength() + a.getSize();
+
+        if (speedStrength > 35) {
+            result += 6;
+        } else if (speedStrength > 32) {
+            result += 4.5;
+        } else if (speedStrength > 30) {
+            result += 3.5;
+        } else if (speedStrength > 28) {
+            result += 2;
+        } else if (speedStrength > 25) {
+            result += 1;
+        } else if (strengthSize > 35) {
+            result += 3.5;
+        } else if (strengthSize > 32) {
+            result += 2.5;
+        } else if (strengthSize > 30) {
+            result += 1.5;
+        } else if (strengthSize > 28) {
+            result += 1;
+        } else if (strengthSize > 26) {
+            result += .5;
+        } else if (a.getSpeed() > 18) {
+            result += 3;
+        } else if (a.getSpeed() > 16) {
+            result += 2;
+        } else if (a.getSpeed() > 14) {
+            result += 2;
+        }
+        return result;
+    }
+
+    private double maybeLowerInidividualDefense(Player a) {
+        double result = 0d;
+        int speedStrength = a.getStrength() + a.getSpeed();
+
+        if (speedStrength < 4) {
+            result += 5;
+        } else if (speedStrength < 7) {
+            result += 4;
+        } else if (speedStrength < 10) {
+            result += 3;
+        } else if (speedStrength < 14) {
+            result += 2;
+        } else if (speedStrength < 18) {
+            result += 1;
+        }
+
+        if (a.getSize() > 13) {
+            if(a.getStrength() < 4) { result += 5; }
+            else if(a.getStrength() < 6) { result += 4; }
+            else if(a.getStrength() < 8) { result += 3; }
+            else if(a.getStrength() < 10) { result += 2; }
+        }
+
+        if (a.getSize() < 8) {
+            if(a.getSpeed() < 4) { result += 5; }
+            else if(a.getSpeed() < 6) { result += 4; }
+            else if(a.getSpeed() < 8) { result += 3; }
+            else if(a.getSpeed() < 10) { result += 2; }
+        }
+        return result;
+    }
+
+
 }
--- src/test/java/software/daveturner/gametimejpa/calc/IndividualDefenseSkillCalculatorTest.java
+++ src/test/java/software/daveturner/gametimejpa/calc/IndividualDefenseSkillCalculatorTest.java
@@ -1,6 +1,7 @@
 package software.daveturner.gametimejpa.calc;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class IndividualDefenseSkillCalculatorTest extends SkillSetCalculatorUnitTest{
 
@@ -8,5 +9,24 @@ public class IndividualDefenseSkillCalculatorTest extends SkillSetCalculatorUnit
     public void setup() {
         calc = new IndividualDefenseSkillCalculator();
         player  = BASE_PLAYER();
+    }
+
+    @Test
+    public void ensureAverageIndividualDefenseReturnsExpected() {
+        assertPlayer(AVERAGE_SKILLSET, calc);
+    }
+
+    @Test
+    public void ensureStrongAndFastHasHighIndividualDefense() {
+        player.setStrength(18);
+        player.setSpeed(18);
+        assertPlayer(16d, calc);
+    }
+
+    @Test
+    public void ensureWeakBigGuysLowIndividualDefense() {
+        player.setStrength(5);
+        player.setSize(18);
+        assertPlayer(5.0d, calc);
     }
 }
